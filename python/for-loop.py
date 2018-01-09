d2a543d056840af785176ec448b8470bb57f3ea7
Aaron Britton <aaron.britton@hotmail.com>
2018-01-09 20:45:03 +0000

    Added initial python files

---
+++ python/for-loop.py
@@ -0,0 +1,16 @@
+array = [1,2,4,6,7,9,34]
+
+for number in array:
+    print number
+
+# Prints out 0, 1, 2, 3, 4, 5
+for ii in range(6):
+    print ii
+
+# Prints out 4, 5, 6
+for ii in range(4, 7):
+    print ii
+
+# Prints out 4, 6, 8
+for ii in range(4, 9, 2):
+    print ii
---
+++ python/if-statements.py
@@ -0,0 +1,13 @@
+inputvalue = 2
+
+if inputvalue == 2:
+    print "Number!"
+else:
+    print "Non-numeric"
+
+secondvalue = 'a'
+
+if secondvalue == 'abc':
+    print "Equal"
+else :
+    print "Different"
