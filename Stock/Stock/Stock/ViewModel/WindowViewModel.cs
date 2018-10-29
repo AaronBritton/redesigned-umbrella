ddb7035e900e2b98e36a6f9d16472c253ffb0ce8
Aaron Britton <aaron.britton@hotmail.com>
2018-10-29 20:06:18 +0000

    50 points to Hufflepuff!

---
+++ Stock/Stock/Stock/ViewModel/WindowViewModel.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Britton.Stock
+{
+    /// <summary>
+    /// The View Model for the custom flat window
+    /// </summary>
+    public class WindowViewModel : BaseViewModel
+    {
+
+        #region Private Members
+
+        /// <summary>
+        /// The window this view model controls
+        /// </summary>
+        private Window mWindow;
+
+        /// <summary>
+        /// The margin around the window to allow for a drop shadow
+        /// </summary>
+        private int mOuterMarginSize = 10;
+
+        /// <summary>
+        /// The radius of the edges of the window
+        /// </summary>
+        private int mWindowRadius = 10;
+
+        #endregion
+
+        #region Public Members
+
+        /// <summary>
+        /// The size of the resize border around the window (how far in points will the mouse change to resizer)
+        /// </summary>
+        public int ResizeBorder { get; set; } = 6;
+
+        /// <summary>
+        /// The size of the resize border around the window, taking into account the outer margin
+        /// </summary>
+        public Thickness ResizeBorderThickness { get { return new Thickness(ResizeBorder + OuterMarginSize); } }
+
+        /// <summary>
+        /// The margin around the window to allow for a drop shadow
+        /// </summary>
+        public int OuterMarginSize
+        {
+            get
+            {
+                return mWindow.WindowState == WindowState.Maximized ? 0 : mOuterMarginSize;
+            }
+            set
+            {
+                mOuterMarginSize = value;
+            }
+        }
+
+
+        /// <summary>
+        /// The margin Thickness around the window to allow for a drop shadow
+        /// </summary>
+        public Thickness OuterMarginSizeThickness { get { return new Thickness(OuterMarginSize); } }
+
+        /// <summary>
+        /// The radius of the edges of the window
+        /// </summary>
+        public int WindowRadius
+        {
+            get
+            {
+                return mWindow.WindowState == WindowState.Maximized ? 0 : mWindowRadius;
+            }
+            set
+            {
+                mWindowRadius = value;
+            }
+        }
+
+
+        /// <summary>
+        /// The Corner Radius of the edges of the window
+        /// </summary>
+        public CornerRadius WindowCornerRadius { get { return new CornerRadius(WindowRadius); } }
+
+        /// <summary>
+        /// The height of the title bar / caption of the window
+        /// </summary>
+        public int TitleHeight { get; set; } = 42;
+
+        /// <summary>
+        /// The height of the title bar / caption of the window
+        /// </summary>
+        public GridLength TitleHeightGridLength { get { return new GridLength(TitleHeight + ResizeBorder); } }
+
+        /// <summary>
+        /// The smallest width the window can go
+        /// </summary>
+        public double WindowMinimumWidth { get; set; } = 400;
+
+        /// <summary>
+        /// The smallest height the window can go
+        /// </summary>
+        public double WindowMinimumHeight { get; set; } = 400;
+
+        /// <summary>
+        /// The padding of the inner content of the main window
+        /// </summary>
+        public int InnerContent { get; set; } = 6;
+
+        /// <summary>
+        /// The padding of the inner content of the main window
+        /// </summary>
+        public Thickness InnerContentThickness { get { return new Thickness(InnerContent); } }
+
+        #endregion
+
+        #region Commands
+
+        /// <summary>
+        /// The command to minimize the window
+        /// </summary>
+        public ICommand MinimizeCommand { get; set; }
+
+        /// <summary>
+        /// The command to maximize the window
+        /// </summary>
+        public ICommand MaximizeCommand { get; set; }
+
+        /// <summary>
+        /// The command to close the window
+        /// </summary>
+        public ICommand CloseCommand { get; set; }
+
+        /// <summary>
+        /// The command to show the system menu of the window
+        /// </summary>
+        public ICommand SystemMenuCommand { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="window"></param>
+        public WindowViewModel(Window window)
+        {
+            mWindow = window;
+
+            // Listen out for the window resizing
+            mWindow.StateChanged += (sender, e) =>
+            {
+                // Fire off events for all properties that are effected by a resize
+                OnPropertyChanged(nameof(ResizeBorderThickness));
+                OnPropertyChanged(nameof(OuterMarginSize));
+                OnPropertyChanged(nameof(OuterMarginSizeThickness));
+                OnPropertyChanged(nameof(WindowRadius));
+                OnPropertyChanged(nameof(WindowCornerRadius));
+            };
+
+            // Create commands
+            MinimizeCommand = new RelayCommand(() => mWindow.WindowState = WindowState.Minimized);
+            MaximizeCommand = new RelayCommand(() => mWindow.WindowState ^= WindowState.Maximized);
+            CloseCommand = new RelayCommand(() => mWindow.Close());
+            SystemMenuCommand = new RelayCommand(() => SystemCommands.ShowSystemMenu(mWindow, GetMousePosition()));
+            
+        }
+
+        #endregion
+
+        #region Private Helpers
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        }
+
+        /// <summary>
+        /// Gets the current mouse position on the screen
+        /// </summary>
+        /// <returns></returns>
+        private static Point GetMousePosition()
+        {
+            Win32Point w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+            return new Point(w32Mouse.X, w32Mouse.Y);
+        }
+
+        #endregion
+    }
+}
--- noty_examples/js/notynoti.js
+++ noty_examples/js/notynoti.js
@@ -1,3 +1,18 @@
+Noty.overrideDefaults({
+  callbacks: {
+    onTemplate: function() {
+      if (this.options.type === 'reply'){
+        this.barDom.innerHTML = '<div class="my-custom-template noty_body">';
+        this.barDom.innerHTML += '<div class="noty-header">' + this.options.category + '</a> <span class="noty-bull">&bull;</span></div>';
+        this.barDom.innerHTML += '<h3 class="from-name">' + this.options.from + '</h3>';
+        this.barDom.innerHTML += '<p class="noty-reply">' + this.options.text + '</p>';
+        this.barDom.innerHTML += '<img src="' + this.options.avatar + '">';
+        this.barDom.innerHTML += '<div>';
+      }
+    }
+  }
+})
+
 // Default Noty Notification in top-right corner of screen
 function defaultNoty(){
   new Noty({
@@ -5,7 +20,7 @@ function defaultNoty(){
   }).show();
 }
 
-// Success Noty Notification
+// Success Noty Notification in top-right corner of screen
 function successNoty(){
   new Noty({
     text: 'Success Notification - Click to acknowledge me',
@@ -21,5 +36,18 @@ function timeouttrNoty(){
     text: 'Timeout Notification',
     layout: 'topRight',
     timeout: 3000
+  }).show();
+}
+
+// Noty Notification (with added Image) in the top-right corner of screen
+function imageNoty(){
+  new Noty({
+    text: 'Image Notification',
+    layout: 'topRight',
+    type: 'reply',
+    category: 'Friend Request',
+    date: Date.now(),
+    from: 'Dr Patrick Wang',
+    avatar: 'https://avatars3.githubusercontent.com/u/271912?v=4&s=460'
   }).show();
 }
